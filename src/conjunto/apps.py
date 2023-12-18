ced9d208d153f4e81f953b8b0646f2af42ff5fe5
Christian González <christian.gonzalez@nerdocs.at>
2023-12-19 00:41:00 +0100

    load conjunto components at startup

--- src/conjunto/apps.py
+++ src/conjunto/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ConjuntoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "conjunto"
+
+    def ready(self):
+        from . import components  # noqa
