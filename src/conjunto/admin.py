0ba1e4e415218c9a33af7200c95fc4af509a2f39
Christian González <christian.gonzalez@nerdocs.at>
2024-02-11 20:08:54 +0100

    add Vendor model to conjunto

--- src/conjunto/admin.py
+++ src/conjunto/admin.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.contrib import admin
 from django.apps import apps
 
+from .models import Vendor
+
 
 class SettingsAdmin(admin.ModelAdmin):
     pass
@@ -9,3 +11,9 @@ class SettingsAdmin(admin.ModelAdmin):
 
 settings_model = apps.get_model(settings.SETTINGS_MODEL)
 admin.site.register(settings_model, SettingsAdmin)
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return super().has_add_permission(request) and not Vendor.objects.exists()
