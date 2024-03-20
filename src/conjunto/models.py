f40d63b66ddbeeea39644045f5f4b1baed8fbbb7
Christian González <christian.gonzalez@nerdocs.at>
2024-03-20 12:49:36 +0100

    use gettext_lazy for model translations

--- src/conjunto/models.py
+++ src/conjunto/models.py
@@ -2,7 +2,7 @@ from django.core.exceptions import MultipleObjectsReturned
 from django.db import models, IntegrityError
 from django.utils import timezone
 from versionfield import VersionField
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
 
 
