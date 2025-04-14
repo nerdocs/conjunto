4f0bfe3d75dc5fd5cabfa52dd5679435f2f9e909
Christian González <christian.gonzalez@nerdocs.at>
2025-04-14 13:16:26 +0200

    fix (the x-th time...) the upload of files life cycle

--- src/conjunto/fields.py
+++ src/conjunto/fields.py
@@ -1,3 +1,7 @@
+import datetime
+import os
+
+from django.core.files.utils import validate_file_name
 from django.db.models import FileField, ImageField
 
 from .files.storage import ProtectedFileSystemStorage
@@ -5,8 +9,30 @@ from .files.storage import ProtectedFileSystemStorage
 
 class ProtectedFileField(FileField):
     def __init__(self, **kwargs):
-        kwargs["storage"] = ProtectedFileSystemStorage()
+        # kwargs["storage"] = ProtectedFileSystemStorage()
         super().__init__(**kwargs)
+
+    def generate_filename(self, instance, filename):
+        if callable(self.upload_to):
+            filename = self.upload_to(instance, filename)
+        else:
+            dirname = datetime.datetime.now().strftime(str(self.upload_to))
+            filename = os.path.join(dirname, filename)
+        filename = validate_file_name(filename, allow_relative_path=True)
+        print(self.storage.generate_filename(filename))
+        return self.storage.generate_filename(filename)
+
+    def pre_save(self, instance, add):
+        """
+        Ensure Django saves the full relative path (including upload_to).
+        """
+        file = super().pre_save(instance, add)
+
+        if file and hasattr(file, "name"):
+            # Ensure that the stored file path includes the correct subdirectory
+            if not file.name.startswith(self.upload_to):
+                file.name = f"{self.upload_to}/{file.name}"
+        return file
 
 
 class ProtectedImageField(ImageField):
--- src/conjunto/files/storage.py
+++ src/conjunto/files/storage.py
@@ -13,3 +13,18 @@ class ProtectedFileSystemStorage(FileSystemStorage):
         kwargs["location"] = settings.PROTECTED_MEDIA_ROOT
         kwargs["base_url"] = settings.PROTECTED_MEDIA_URL
         super().__init__(*args, **kwargs)
+
+    def get_valid_name(self, name):
+        return super().get_valid_name(name)
+
+    def save(self, name, content, max_length=None):
+        print(
+            f"ProtectedFileSystemStorage: Saving file: {name} with content length: "
+            f"{len(content)} bytes"
+        )
+        return super().save(name, content, max_length)
+
+    def path(self, name):
+        print(f"ProtectedFileSystemStorage: Path requested: {name}")  # Check if full
+        # path is correct
+        return super().path(name)
