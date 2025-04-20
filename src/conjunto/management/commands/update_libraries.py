f71373a1ee9f3059257b75e76e08086d7c93bfbe
Christian González <christian.gonzalez@nerdocs.at>
2025-04-20 21:01:51 +0200

    Fix reference to minified Tabler CSS file

--- src/conjunto/management/commands/update_libraries.py
+++ src/conjunto/management/commands/update_libraries.py
@@ -129,7 +129,7 @@ class Command(BaseCommand):
                 "w",
             ),
             (
-                "css/tabler.css",
+                "css/tabler.min.css",
                 f"{tabler_path}/css/tabler.min.css",
                 "w",
             ),
